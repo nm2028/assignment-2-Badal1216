888aec77954ca016f31163738a9c4cd45799db85
LENOVO <badal.bhatt_cs18@gla.ac.in>
2020-03-27 23:17:37 +0530

    create a Node class of problem3

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,6 +6,34 @@
  */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    private int data;
+    private Student student;
+    private Node next;
+
+    public Node(int data, Student student) {
+        this.data = data;
+        this.student = student;
+        next = null;
+    }
+
+    public Student getData() {
+        return student;
+    }
+
+    public void setData(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 
 }
